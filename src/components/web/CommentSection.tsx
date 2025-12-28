bcd02d717b0a7a26918a24b5f5e7d96fa3f48fe6
Anupam <itsmrad@gmail.com>
2025-12-28 23:02:32 +0530

    Bug Fix : fixed Double Loading Comment Component

--- src/components/web/CommentSection.tsx
+++ src/components/web/CommentSection.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Loader2, MessageSquare } from "lucide-react";
 import { Card, CardContent, CardHeader } from "../ui/card";
@@ -10,7 +10,7 @@ import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
-import { Preloaded, useMutation, usePreloadedQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import z from "zod";
 import { toast } from "sonner";
@@ -18,50 +18,56 @@ import { useTransition } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Separator } from "../ui/separator";
 
-export function CommentSection( props: {preloadedComments: Preloaded<typeof api.comments.getCommentsByPostId>}) {
-  const [isPending, startTransition] = useTransition()
-  const params = useParams<{postId: Id<"posts">}>();
-  const createComment = useMutation(api.comments.createComment)
-  const data = usePreloadedQuery(props.preloadedComments)
+import { Preloaded, usePreloadedQuery } from "convex/react";
 
+export function CommentSection(props: {
+  preloadedComments: Preloaded<typeof api.comments.getCommentsByPostId>;
+}) {
+  const params = useParams<{ postId: Id<"posts"> }>();
+  const data = usePreloadedQuery(props.preloadedComments);
+  const [isPending, startTransition] = useTransition();
+
+  const createComment = useMutation(api.comments.createComment);
   const form = useForm({
     resolver: zodResolver(commentSchema),
     defaultValues: {
       body: "",
       postId: params.postId,
-    }
-  })
+    },
+  });
 
-  const onSubmit = async (data: z.infer<typeof commentSchema>) => {
-    startTransition( async () => {
+  async function onSubmit(data: z.infer<typeof commentSchema>) {
+    startTransition(async () => {
       try {
         await createComment(data);
-        form.reset()
-        toast.success("Comment posted")
+        form.reset();
+        toast.success("Comment posted");
       } catch {
-        toast.error("Failed to create comment")
+        toast.error("Failed to create post");
       }
-    })
+    });
+  }
+
+  if (data === undefined) {
+    return <p>loading...</p>;
   }
 
   return (
     <Card>
       <CardHeader className="flex flex-row items-center gap-2 border-b">
-        <MessageSquare className="size-4" />
-        <h2 className="text-xl font-bold">
-          {data?.length} Comments
-        </h2>
+        <MessageSquare className="size-5" />
+        <h2 className="text-xl font-bold">{data.length} Comments</h2>
       </CardHeader>
       <CardContent className="space-y-8">
         <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-          <Controller 
+          <Controller
             name="body"
             control={form.control}
-            render={({ field, fieldState}) => (
+            render={({ field, fieldState }) => (
               <Field>
                 <FieldLabel>Full Name</FieldLabel>
-                <Textarea 
-                  aria-invalid = {fieldState.invalid} 
+                <Textarea
+                  aria-invalid={fieldState.invalid}
                   placeholder="Share your thoughts"
                   {...field}
                 />
@@ -71,49 +77,51 @@ export function CommentSection( props: {preloadedComments: Preloaded<typeof api.
               </Field>
             )}
           />
+
           <Button disabled={isPending}>
             {isPending ? (
               <>
                 <Loader2 className="size-4 animate-spin" />
-                <span>Posting Comment...</span>
+                <span>Loading...</span>
               </>
             ) : (
-              <span>Post Comment</span>
+              <span>Comment</span>
             )}
           </Button>
         </form>
-        <Separator />
-        {data === undefined && (
-          <p>Loading comments...</p>
-        )}
-        {data?.length === 0 && (
-          <p>No comments yet</p>
-        )}
-        {(data?.length ?? 0) > 0 && (
-          <section className="space-y-6">
-            {data?.map((comment) => (
-              <div key={comment._id} className="flex gap-4">
-                <Avatar className="size-10 shrink-0">
-                  <AvatarImage src={`https://avatar.vercel.sh/${comment.authorName}`} alt={comment.authorName} />
+
+        {data?.length > 0 && <Separator />}
+
+        <section className="space-y-6">
+          {data?.map((comment) => (
+            <div key={comment._id} className="flex gap-4">
+              <Avatar className="size-10 shrink-0">
+                <AvatarImage
+                  src={`https://avatar.vercel.sh/${comment.authorName}`}
+                  alt={comment.authorName}
+                />
                 <AvatarFallback>
                   {comment.authorName.slice(0, 2).toUpperCase()}
                 </AvatarFallback>
-                </Avatar>
-                <div className="flex-1 space-y-1">
-                  <div className="flex items-center justify-between">
-                    <p className="font-semibold text-sm">{comment.authorName}</p>
-                    <p className="text-xs text-muted-foreground">
-                      {new Date(comment._creationTime).toLocaleDateString()}
-                    </p>
-                  </div>
-                  <p className="text-sm text-foreground/90 whitespace-pre-wrap leading-relaxed">{comment.body}</p>
+              </Avatar>
+              <div className="flex-1 space-y-1">
+                <div className="flex items-center justify-between">
+                  <p className="font-semibold text-sm">{comment.authorName}</p>
+                  <p className="text-muted-foreground text-xs">
+                    {new Date(comment._creationTime).toLocaleDateString(
+                      "en-US"
+                    )}
+                  </p>
                 </div>
-              </div>
-            ))}
-          </section>
-        )}
-      </CardContent>
 
+                <p className="text-sm text-foreground/90 whitespace-pre-wrap leading-relaxed">
+                  {comment.body}
+                </p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </CardContent>
     </Card>
-  )
+  );
 }
